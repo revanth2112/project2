5533413598fa4352d0f705fe4886a8d1ffb82a03
revanth2112 <106611451+revanth2112@users.noreply.github.com>
2023-09-12 10:15:45 +0530

    Update settings.py

--- new/settings.py
+++ new/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-49&&l2d^^*6ay1k!ce*x(!xz5qx_$ri^h&1=oinmxs)nw4j2ba'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
